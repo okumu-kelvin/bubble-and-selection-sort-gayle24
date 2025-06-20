4e55cccf0b383bfb68b2e0566ee858df23eedcc6
Gayle Amani <amaniabbie24@gmail.com>
2025-06-20 11:18:49 +0300

    bubble sort

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,14 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-    pass
+
+    #set the length of the input list
+    n = len(unsorted_list);
+
+    # iterate through the list n-1 times for each value
+    for i in range(n-1):
+        for j in range(n-i-1): #for each time the list is sorted, the next iteration will omit the largest value at the end
+            # for two adjacent values, they are swapped if first value is larger than the second
+            if unsorted_list[j] > unsorted_list[j+1]: 
+                unsorted_list[j], unsorted_list[j+1] = unsorted_list[j+1], unsorted_list[j]
+    return unsorted_list
+
