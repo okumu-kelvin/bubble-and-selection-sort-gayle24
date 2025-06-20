e548d0e44134a19a95d957978aeb428968963e4b
Gayle Amani <amaniabbie24@gmail.com>
2025-06-20 11:45:11 +0300

    selection sort

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,18 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
-    pass
+    n = len(arr)
+    # set outer loop to limit how many times the inner loop will run
+    for i in range(n-1):
+        # set the minimum value in the list to be the first index
+        minim = i
+        # inner loop checks all values in the list if they are smaller than the minimum value
+        for j in range(i+1, n):
+            # if current value is less than minimum value, it is set as the new minimum value
+            if arr[j] < arr[minim]:
+                minim = j
+        # place the minimum value correctly before all other values
+        arr[i], arr[minim] = arr[minim], arr[i]
+    return arr
+
+# test run
+# print(selection_sort([2,8,4,3,1]))
